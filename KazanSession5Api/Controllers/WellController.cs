28796d8f08061d4635985234bf04a238a8c0a8d4
HngWu <hngwutan@gmail.com>
2024-10-13 10:57:30 +0800

    Template Routes

---
+++ KazanSession5Api/Controllers/WellController.cs
@@ -0,0 +1,76 @@
+﻿using KazanSession5Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KazanSession5Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WellController : Controller
+    {
+
+        public class TempWell
+        {
+            public long Id { get; set; }
+
+            public long WellTypeId { get; set; }
+
+            public string WellName { get; set; } = null!;
+
+            public long GasOilDepth { get; set; }
+
+            public long Capacity { get; set; }
+
+        }
+
+
+        Wsc2019Session5Context context = new Wsc2019Session5Context();
+
+        [HttpGet]
+        public IActionResult GetWell()
+        {
+            try
+            {
+                var wells = context.Wells.ToList();
+
+
+                return Ok(wells);
+            }
+            catch (Exception)
+            {
+
+                return NotFound();
+            }
+            
+        }
+
+        [HttpPost]
+        public IActionResult CreateWell(TempWell well)
+        {
+            try
+            {
+                var newWell = new Well
+                {
+                    Id = well.Id,
+                    WellTypeId = well.WellTypeId,
+                    WellName = well.WellName,
+                    GasOilDepth = well.GasOilDepth,
+                    Capacity = well.Capacity
+                };
+
+                context.Wells.Add(newWell);
+                context.SaveChanges();
+
+                return Ok();
+            }
+            catch (Exception)
+            {
+
+                return NotFound();
+            }
+        }
+
+
+
+
+    }
+}
---
+++ KazanSession5Api/Models/RockType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KazanSession5Api.Models;
+
+public partial class RockType
+{
+    public long Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string BackgroundColor { get; set; } = null!;
+
+    public virtual ICollection<WellLayer> WellLayers { get; set; } = new List<WellLayer>();
+}
