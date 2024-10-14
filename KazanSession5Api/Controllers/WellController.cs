75cb5f621b883f2743248cb5ae94b51631f35d2d
HngWu <hngwutan@gmail.com>
2024-10-14 15:31:14 +0800

    Update WellController.cs

--- KazanSession5Api/Controllers/WellController.cs
+++ KazanSession5Api/Controllers/WellController.cs
@@ -1,28 +1,37 @@
 ﻿using KazanSession5Api.Models;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace KazanSession5Api.Controllers
 {
+    public class tempWellLayer
+    {
+        public int Id { get; set; }
+        public int WellId { get; set; }
+        public int RockTypeId { get; set; }
+        public int StartPoint { get; set; }
+        public int EndPoint { get; set; }
+        public string RockName { get; set; }
+        public string BackgroundColor { get; set; }
+    }
+
+    public class tempWell
+    {
+        public int Id { get; set; }
+        public int WellTypeId { get; set; }
+        public string WellName { get; set; }
+        public int GasOilDepth { get; set; }
+        public int Capacity { get; set; }
+        public List<tempWellLayer> WellLayers { get; set; } = new List<tempWellLayer>();
+        public string WellTypeName { get; set; }
+    }
+
     [ApiController]
     [Route("[controller]")]
     public class WellController : Controller
     {
-
-        public class TempWell
-        {
-            public long Id { get; set; }
-
-            public long WellTypeId { get; set; }
-
-            public string WellName { get; set; } = null!;
-
-            public long GasOilDepth { get; set; }
-
-            public long Capacity { get; set; }
-
-        }
-
-
         Wsc2019Session5Context context = new Wsc2019Session5Context();
 
         [HttpGet]
@@ -30,21 +39,73 @@ namespace KazanSession5Api.Controllers
         {
             try
             {
-                var wells = context.Wells.ToList();
-
+                var wells = context.Wells
+                    .Select(x => new
+                    {
+                        x.Id,
+                        x.WellTypeId,
+                        x.WellName,
+                        x.GasOilDepth,
+                        x.Capacity,
+                        wellLayers = x.WellLayers.Select(y => new
+                        {
+                            y.Id,
+                            y.WellId,
+                            y.RockTypeId,
+                            y.StartPoint,
+                            y.EndPoint,
+                            rockName = y.RockType.Name,
+                            rockColor = y.RockType.BackgroundColor
+                        }),
+                        wellTypeName = x.WellType.Id
+                    });
 
                 return Ok(wells);
             }
             catch (Exception)
             {
-
                 return NotFound();
             }
-            
         }
 
-        [HttpPost]
-        public IActionResult CreateWell(TempWell well)
+        [HttpPost("create")]
+        public IActionResult CreateWell(tempWell well)
+        {
+            try
+            {
+                var newWell = new Well
+                {
+                    WellTypeId = well.WellTypeId,
+                    WellName = well.WellName,
+                    GasOilDepth = well.GasOilDepth,
+                    Capacity = well.Capacity
+                };
+                context.Wells.Add(newWell);
+                context.SaveChanges();
+
+                var wellLayers = well.WellLayers.Select(x => new WellLayer
+                {
+                    WellId = newWell.Id,
+                    RockTypeId = x.RockTypeId,
+                    StartPoint = x.StartPoint,
+                    EndPoint = x.EndPoint
+                })
+                    .ToList();
+
+
+                context.WellLayers.AddRange(wellLayers);
+                context.SaveChanges();
+
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost("update")]
+        public IActionResult UpdateWell(tempWell well)
         {
             try
             {
@@ -57,20 +118,27 @@ namespace KazanSession5Api.Controllers
                     Capacity = well.Capacity
                 };
 
+                var wellLayers = well.WellLayers.Select(x => new WellLayer
+                {
+                    Id = x.Id,
+                    WellId = x.WellId,
+                    RockTypeId = x.RockTypeId,
+                    StartPoint = x.StartPoint,
+                    EndPoint = x.EndPoint
+                })
+                    .ToList();
+
+
                 context.Wells.Add(newWell);
+                context.WellLayers.AddRange(wellLayers);
                 context.SaveChanges();
 
                 return Ok();
             }
             catch (Exception)
             {
-
                 return NotFound();
             }
         }
-
-
-
-
     }
 }
